0eff0324ed499e31aaeb04c48750ff3b67d53cdc
oslo2015 <475920112@qq.com>
2015-11-30 18:49:02 +0800

    first commit

---
+++ prior_new.tcl
@@ -0,0 +1,7 @@
+set     k				4
+set     coreNum         [expr $k * $k / 4]
+set     podNum	      	$k
+set     eachPodNum		[expr $k / 2]
+set     hostNum			[expr $k * $k * $k /4]
+set		TAGSEC			1
+set		runningTAGSEC	0
