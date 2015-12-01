eca1f52f314e2106648658ac775ec3476f6aee15
oslo2015 <475920112@qq.com>
2015-12-01 14:55:33 +0800

    添加 flow base perl 程序

---
+++ perl_prior_test8.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+
+
+$logFile="/home/oslo/simu/ns2_test/result-pror-7.txt";
+$tclFile="/home/oslos/myGit/ns2/prior_test7.tcl";
+
+#$AllocInput="/home/hadoop/myGit/ns2/w_file.tcl";
+
+$AwkFile = "/home/hadoop/myGit/ns2/awkpacketLoss.awk";
+$TraceFile = "/home/hadoop/simu/prior_test7.tr";
+
+
+system("rm $logFile");
+
+#system("rm /home/hadoop/loaDis.txt");
+$f2=$logFile;
+$totalJob=3;
+$tag=1;
+$runTime=3;
+
+for($i=1; $i<=$runTime; $i=$i+1)
+{
+	#system("echo	TEST  >> $f2");
+	#system("echo	total : $total   tag : $tag  >> $f2");
+	#system("echo	Number : $i >> $f2");
+	#system("ns $AllocInput 200 >> $f2");
+	
+	system("ns $tclFile $totalJob $totalJob 2  >> $f2");
+	#system("awk -f $AwkFile $TraceFile >> $f2");
+	system("ns $tclFile $totalJob 0 1 >> $f2");
+	#system("awk -f $AwkFile $TraceFile >> $f2");
+
+#print "\n";
+}
+
+
+
+
