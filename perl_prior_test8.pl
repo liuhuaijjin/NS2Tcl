8e24395ca8ad165a5013953051707a57057e845f
oslo2015 <475920112@qq.com>
2015-12-05 14:15:38 +0800

    设置对比实验

--- perl_prior_test8.pl
+++ perl_prior_test8.pl
@@ -30,10 +30,13 @@ for($j=1; $j<=8;$j=$j+1)
 		#system("echo	total : $total   tag : $tag  >> $f2");
 		#system("echo	Number : $i >> $f2");
 		#system("ns $AllocInput 200 >> $f2");
-		system("ns $tclFile $totalJob $totalJob		2 1 0 >> $f2");
-		system("ns $tclFile $totalJob $totalJob		1 0 0 >> $f2");
-		system("ns $tclFile $totalJob $totalJob		1 0 1 >> $f2");
-		system("ns $tclFile $totalJob 0 			1 0 1 >> $f2");
+		# 		ns tclFile	jobNum		优先级	文件读取方式	多路径方式(flow/spray)	路径方式(单/多)
+		system("ns $tclFile $totalJob	$totalJob	2	1	0 >> $f2");
+		system("ns $tclFile $totalJob	0			1	1	0 >> $f2");
+		system("ns $tclFile $totalJob	$totalJob	1	0	0 >> $f2");
+		system("ns $tclFile $totalJob	0			1	0	0 >> $f2");
+		system("ns $tclFile $totalJob	$totalJob	1	0	1 >> $f2");
+		system("ns $tclFile $totalJob	0			1	0	1 >> $f2");
 		#system("awk -f $AwkFile $TraceFile >> $f2");
 	}
 }
@@ -43,11 +46,13 @@ for($j=1; $j<=8;$j=$j+1)
 # argv2		HowToReadPoint	-- 1代表读取文件	-- 2代表随机产生
 # argv3		isflowBased		-- 1代表flowBased	-- 0代表packetBased
 # argv4		isSinglePath	-- 1代表设置成单路径
-# 进行4次实验 
+# 进行6次实验
 	# 1 : flow		优先级
-	# 2 : spray		优先级
-	# 3 : single	优先级
-	# 4 : single	无优先级
+	# 2 : flow		无优先级
+	# 3 : spray		优先级
+	# 4 : spray		无优先级
+	# 5 : single	优先级
+	# 6 : single	无优先级
 #每次实验输出格式
 # queueNum :
 #	job1	job2	...		jobN	totalTime
