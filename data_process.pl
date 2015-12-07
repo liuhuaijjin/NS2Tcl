ef933f8928eeb829ab4b4d5d399c3a82317c1efe
oslo2015 <475920112@qq.com>
2015-12-07 10:27:49 +0800

    修改bug

--- data_process.pl
+++ data_process.pl
@@ -3,14 +3,13 @@
 
 $inputFile="/home/oslo/simu/result-pror-8.txt";
 $scriptFile="/home/oslo/simu/ns2program/newAvg.awk";
-$outputFile="/home/oslo/simu/testResult/expr_data.txt"
+$outputFile="/home/oslo/simu/testResult/expr_data.txt";
 
 for($j=1; $j<=8;$j=$j+1)
 {
 	$inputFile="/home/oslo/simu/result-pror-8-$j.txt";
-	$outputFile="/home/oslo/simu/testResult/expr_data-$j.txt"
+	$outputFile="/home/oslo/simu/testResult/expr_data-$j.txt";
 	system("awk -f $scriptFile $inputFile >> $outputFile");
-	
 }
 
 #system("shutdown -h now");
